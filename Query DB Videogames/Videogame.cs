df0680ad93a170b609a70d6810d67e0911478fbf
niccov <niccoventuri27@gmail.com>
2023-09-21 17:08:00 +0200

    Creazione classe VideogameManager

--- Query DB Videogames/Videogame.cs
+++ Query DB Videogames/Videogame.cs
@@ -8,6 +8,11 @@ namespace Query_DB_Videogames
 {
     public class Videogame
     {
+        private long v1;
+        private string v2;
+        private string v3;
+        private DateTime dateTime;
+
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Overview { get; private set; }
@@ -20,6 +25,14 @@ namespace Query_DB_Videogames
             Name = name;
             Overview = overview;
             Date = date;
+        }
+
+        public Videogame(long v1, string v2, string v3, DateTime dateTime)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.dateTime = dateTime;
         }
     }
 }
--- Query DB Videogames/VideogameManager.cs
+++ Query DB Videogames/VideogameManager.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Query_DB_Videogames
 {
-    internal class VideogameManager
+    public static class VideogameManager
     {
+        private static string connectionString = "Data Source=localhost;Initial Catalog=videogames;Integrated Security=True";
+
+        public static List<Videogame> GetVideogames()
+        {
+            List<Videogame> videogame = new List<Videogame>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT id, name, overview, release_date FROM videogames;";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    using (SqlDataReader data = cmd.ExecuteReader())
+                    {
+                        while (data.Read())
+                        {
+                            Videogame videogameReaded = new Videogame(data.GetInt64(0), data.GetString(1), data.GetString(2), data.GetDateTime(3));
+
+                            videogame.Add(videogameReaded);
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return videogame;
+            }
+        }
     }
 }
